df301a4d577f49e9b70178454c99df5bb8e45609
Ahesanali Khanusiya <ahesanali.khanusiya@devitpl.com>
2018-05-16 11:30:17 +0530

    add server.js

---
+++ server.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+
+var app = express();
+
+var indexRouter = require('./routes/index');
+var uploadRouter = require('./routes/uploadFile');
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app.set("port", server_port);
+
+var server = app.listen(app.get("port"), function() {
+  console.log("res_catalog: server is running at port: " + app.get("port"));
+});
+
+server.on('error', onError);
+server.on('listening', onListening);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/public',express.static(path.join(__dirname, 'public')));
+app.use('/uploads',express.static(path.join(__dirname, 'uploads')));
+app.use('/pages',express.static(path.join(__dirname, 'pages')));
+
+app.use('/', indexRouter);
+app.use('/upload', uploadRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
+}
